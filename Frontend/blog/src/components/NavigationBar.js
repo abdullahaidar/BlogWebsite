cc2118665d73a3e0310d4382ccc3c562867076b1
abdullahhaidar <abudehhaidar8@gmail.com>
2021-05-24 13:30:37 +0200

    link homepage

--- Frontend/blog/src/components/NavigationBar.js
+++ Frontend/blog/src/components/NavigationBar.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 
 export const NavigationBar = () => {
-    return (
-        <header className="header">
-        <div className="logo">Blog Project</div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Posts</Link>
-            </li>
-            <li>
-              <Link to='/new'>Add New Post</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      
-    )
+  return (
+    <header className="header">
+      <Link to='/'><div className="logo">Blog Project</div></Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Posts</Link>
+          </li>
+          <li>
+            <Link to='/new'>Add New Post</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+  )
 }
